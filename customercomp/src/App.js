34bab795babeaa0dbeec56dac641cfbe60bce1a2
Crystal Merlin D Souza <crystalmdsouza18@gmail.com>
2024-11-20 23:55:32 +0530

    admin login removed

--- customercomp/src/App.js
+++ customercomp/src/App.js
@@ -1,8 +1,6 @@
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SelectUser from "./SelectUser";
-import AdminLogin from "./AdminLogin";
 import UserLogin from "./UserLogin";
 import AdminDashboard from "./AdminDashboard";
 import RaiseComplaint from "./RaiseComplaint";
@@ -16,11 +14,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SelectUser />} />
-        <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/" element={<UserLogin />} />
         <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/user" element={<UserLogin />} />
         <Route path="/complaint" element={<RaiseComplaint />} />
         <Route path="/status" element={<ComplaintStatus />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
